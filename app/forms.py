61f53fdf7ab4b9f5c7fe72809b330f09cab58749
Bilal Karim <beekay-@users.noreply.github.com>
2017-04-29 15:38:51 -0600

    Code cleanup

--- app/forms.py
+++ app/forms.py
@@ -5,9 +5,7 @@ from wtforms.fields.html5 import EmailField, TelField, IntegerField, DecimalFiel
 
 class EmailForm(Form):
     email = EmailField('Email', [validators.DataRequired(), validators.Email()])
-    telephone = TelField('Phone (optional)', [validators.Length(min=0, max=15)], id="telephone-number")
     name = TextField('Name', [validators.DataRequired(), validators.Length(min=2, max=50)])
     baraat = BooleanField('Baraat')
     walima = BooleanField('Walima')
     numguests = SelectField('Total Number of Guests', choices=[('1','1'), ('2','2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'),  ('8', '8')])
-    comments = TextAreaField('Comments', [validators.DataRequired(), validators.Length(min=10, max=5000)])
